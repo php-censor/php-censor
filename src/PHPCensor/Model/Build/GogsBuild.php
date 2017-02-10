91a8054fdd10b15b7b38fcb2d8f520a9b77a70b9
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-02-10 23:14:07 +0700

    Added highlighting for build config + updated dependencies

--- src/PHPCensor/Model/Build/GogsBuild.php
+++ src/PHPCensor/Model/Build/GogsBuild.php
@@ -8,17 +8,19 @@ namespace PHPCensor\Model\Build;
 class GogsBuild extends RemoteGitBuild
 {
     /**
-    * Get link to commit from Gogs repositorie
+    * Get link to commit from Gogs repository
     */
     public function getCommitLink()
     {
         if ($this->getCommitId() !== 'manual'){
             return $this->getProject()->getReference() . '/commit/' . $this->getCommitId();
         }
+
+        return parent::getCommitLink();
     }
 
     /**
-    * Get link to branch from Gogs repositorie
+    * Get link to branch from Gogs repository
     */
     public function getBranchLink()
     {
