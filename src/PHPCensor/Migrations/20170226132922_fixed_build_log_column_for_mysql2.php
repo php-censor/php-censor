00a2248c93e291b5b83738d727983c6498cf7e42
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-02-26 20:46:24 +0700

    Fixed Build.log column size for MySQL (removed 'NOT NULL')

---
+++ src/PHPCensor/Migrations/20170226132922_fixed_build_log_column_for_mysql2.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class FixedBuildLogColumnForMysql2 extends AbstractMigration
+{
+    public function up()
+    {
+        $adapter = $this->getAdapter();
+        if ($adapter instanceof MysqlAdapter) {
+            $this
+                ->table('build')
+                ->changeColumn('log', MysqlAdapter::PHINX_TYPE_TEXT, ['limit' => MysqlAdapter::TEXT_LONG, 'null' => true])
+                ->save();
+        }
+    }
+
+    public function down()
+    {
+    }
+}
