73363b02f546a77333c6cfccc9bf72ed98b2b657
Stepan Strelets <s.strelec@nikitaonline.ru>
2017-03-26 01:38:49 +0300

    Improve - environment settings on project creation

--- src/PHPCensor/Service/ProjectService.php
+++ src/PHPCensor/Service/ProjectService.php
@@ -84,15 +84,18 @@ class ProjectService
             $project->setGroup($options['group']);
         }
 
-        if (array_key_exists('environments', $options)) {
-            $project->setEnvironments($options['environments']);
-        }
-
         // Allow certain project types to set access information:
         $this->processAccessInformation($project);
 
         // Save and return the project:
-        return $this->projectStore->save($project);
+        /** @var Project $project */
+        $project = $this->projectStore->save($project);
+
+        if (array_key_exists('environments', $options)) {
+            $project->setEnvironments($options['environments']);
+        }
+
+        return $project;
     }
 
     /**
