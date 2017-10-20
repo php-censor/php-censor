53585b2451a12ee72085ffa8ea0b4670000e57a6
Luis Cortés <jluis.cortes.cuevas@gmail.com>
2017-10-20 14:57:42 -0500

    Fix Migrations | Installation Failed

--- src/PHPCensor/Migrations/20171014173348_removed_project_id_from_build_meta.php
+++ src/PHPCensor/Migrations/20171014173348_removed_project_id_from_build_meta.php
@@ -8,6 +8,10 @@ class RemovedProjectIdFromBuildMeta extends AbstractMigration
     {
         $table = $this->table('build_meta');
 
+        if ($table->hasForeignKey('project_id')) {
+            $table->dropForeignKey('project_id');
+        }
+
         if ($table->hasColumn('project_id')) {
             $table
                 ->removeColumn('project_id')
@@ -21,7 +25,14 @@ class RemovedProjectIdFromBuildMeta extends AbstractMigration
 
         if (!$table->hasColumn('project_id')) {
             $table
-                ->addColumn('project_id', 'integer', ['default' => 0])
+                ->addColumn(
+                    'project_id',
+                    'integer', [
+                        'default' => 0,
+                        'after' => 'id'
+                    ]
+                )
+                ->addForeignKey('project_id', 'project', 'id', ['delete'=> 'CASCADE', 'update' => 'CASCADE'])
                 ->save();
         }
     }
