334cc8e19ac51e06906264aa3803a971b6cc4529
Dmitry Khomutov <poisoncorpsee@gmail.com>
2019-01-13 00:32:13 +0700

    Fixed column types for MySQL after Phinx update. Issue #239.

--- src/Migrations/20170223113127_fixed_build_log_column_for_mysql.php
+++ src/Migrations/20170223113127_fixed_build_log_column_for_mysql.php
@@ -1,27 +1,14 @@
 <?php
 
 use Phinx\Migration\AbstractMigration;
-use Phinx\Db\Adapter\MysqlAdapter;
 
 class FixedBuildLogColumnForMysql extends AbstractMigration
 {
     public function up()
     {
-        if ($this->getAdapter() instanceof MysqlAdapter) {
-            $this
-                ->table('build')
-                ->changeColumn('log', 'text', ['limit' => MysqlAdapter::TEXT_LONG])
-                ->save();
-        }
     }
 
     public function down()
     {
-        if ($this->getAdapter() instanceof MysqlAdapter) {
-            $this
-                ->table('build')
-                ->changeColumn('log', 'text')
-                ->save();
-        }
     }
 }
