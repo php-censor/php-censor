bdca7adac501590cde385d1d27040ddef0b82ea1
Dmitry Khomutov <poisoncorpsee@gmail.com>
2019-02-09 18:36:32 +0700

    Fixed behavior of application config option 'email_settings.from_address' for case when 'from_address' like 'test@test.test' without user name.
    Now the addresses like 'test@test.test' will be transform automatically
    to format: 'PHP Censor <test@test.test>'.

---
+++ tests/src/Helper/EmailTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\PHPCensor\Helper;
+
+use PHPCensor\Config;
+use PHPCensor\Helper\Email;
+
+class EmailTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var Config | \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $config;
+
+    /**
+     * @var Email
+     */
+    private $email;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->config = $this->createMock(Config::class);
+        $this->email  = new Email($this->config);
+    }
+
+    public function testGetFrom()
+    {
+        $this->config
+            ->expects($this->once())
+            ->method('get')
+            ->with('php-censor.email_settings.from_address', Email::DEFAULT_FROM)
+            ->willReturn('Test <test@test.test>');
+
+        self::assertEquals([
+            'test@test.test' => 'Test',
+        ], $this->email->getFrom());
+    }
+
+    public function testGetFrom2()
+    {
+        $this->config
+            ->expects($this->once())
+            ->method('get')
+            ->with('php-censor.email_settings.from_address', Email::DEFAULT_FROM)
+            ->willReturn('test@test.test');
+
+        self::assertEquals([
+            'test@test.test' => 'PHP Censor',
+        ], $this->email->getFrom());
+    }
+}
