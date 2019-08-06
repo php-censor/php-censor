5f578bfecfab14d465d66a1c85a97b901ac9d1a5
Mathias Kühn <mathias.kuehn@check24.de>
2019-08-06 07:53:01 +0200

    modified unittests for PhpUnitResult and PluginResult classes
    - combined tests using dataprovider
    - added tests for new PluginClass

--- tests/src/Plugin/Util/BitbucketNotifyPhpUnitResultTest.php
+++ tests/src/Plugin/Util/BitbucketNotifyPhpUnitResultTest.php
@@ -4,7 +4,7 @@ namespace PHPCensor\Plugin\Util;
 
 use PHPUnit\Framework\TestCase;
 
-class BitbucketNotifyPluginResultTest extends TestCase
+class BitbucketNotifyPhpUnitResultTest extends TestCase
 {
     public function dataProvider()
     {
@@ -37,28 +37,28 @@ class BitbucketNotifyPluginResultTest extends TestCase
             ],
             'improved' => [
                 [
-                    'test', 1, 0,
+                    'test', 0, 1,
                 ],
                 [
                     'unchanged' => false,
                     'improved' => true,
                     'degraded' => false,
                     'string' => 'test',
-                    'formattedOutput' => "test       | 1\t=> 0\tgreat success!",
+                    'formattedOutput' => "test       | 0\t=> 1\tgreat success!",
                     'taskDescription' => '',
                 ]
             ],
             'degraded' => [
                 [
-                    'test', 0, 1,
+                    'test', 1, 0,
                 ],
                 [
                     'unchanged' => false,
                     'improved' => false,
                     'degraded' => true,
                     'string' => 'test',
-                    'formattedOutput' => "test       | 0\t=> 1\t!!!!! o_O",
-                    'taskDescription' => 'pls fix test because it has increased from 0 to 1 errors',
+                    'formattedOutput' => "test       | 1\t=> 0\t!!!!! o_O",
+                    'taskDescription' => 'pls fix test because the coverage has decreased from 1 to 0',
                 ]
             ],
         ];
@@ -69,44 +69,14 @@ class BitbucketNotifyPluginResultTest extends TestCase
      * @param array $input
      * @param array $expected
      */
-    public function test__toString(array $input, array $expected)
-    {
-        $sut = new BitbucketNotifyPluginResult($input[0], $input[1], $input[2]);
-        $this->assertEquals($expected['string'], $sut->__toString());
-    }
-
-    /**
-     * @dataProvider dataProvider
-     * @param array $input
-     * @param array $expected
-     */
     public function testFormating(array $input, array $expected)
     {
-        $pluginResult = new BitbucketNotifyPluginResult($input[0], $input[1], $input[2]);
+        $pluginResult = new BitbucketNotifyPhpUnitResult($input[0], $input[1], $input[2]);
         $this->assertEquals($expected['degraded'], $pluginResult->isDegraded());
         $this->assertSame($expected['taskDescription'], $pluginResult->generateTaskDescription());
         $this->assertSame($expected['formattedOutput'], $pluginResult->generateFormattedOutput(10));
         $this->assertEquals($expected['degraded'], $pluginResult->isDegraded());
         $this->assertEquals($expected['improved'], $pluginResult->isImproved());
         $this->assertEquals($expected['unchanged'], $pluginResult->isUnchanged());
-    }
-
-    public function testSetterGetter()
-    {
-        $pluginResult = new BitbucketNotifyPluginResult('noname', 9, 9);
-        $this->assertEquals(false, $pluginResult->isDegraded());
-        $this->assertEquals(false, $pluginResult->isImproved());
-        $this->assertEquals(true, $pluginResult->isUnchanged());
-
-        $pluginResult->setLeft(99);
-        $pluginResult->setRight(199);
-        $pluginResult->setPlugin('BestPluginEver4Cod1ng');
-
-        $this->assertEquals('99', $pluginResult->getLeft());
-        $this->assertEquals(199, $pluginResult->getRight());
-        $this->assertEquals('BestPluginEver4Cod1ng', $pluginResult->getPlugin());
-        $this->assertEquals(true, $pluginResult->isDegraded());
-        $this->assertEquals(false, $pluginResult->isImproved());
-        $this->assertEquals(false, $pluginResult->isUnchanged());
     }
 }
