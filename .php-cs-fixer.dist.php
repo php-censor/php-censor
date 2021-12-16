db9820082a6ea8ea0570671b0e5bf4a00cf2d9b8
Dmitry Khomutov <poisoncorpsee@gmail.com>
2021-12-16 22:26:09 +0700

    PHPCsFixer config fix for @PSR2 and PHP5.6.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,22 +15,13 @@ $config = new Config();
 return $config
     ->setRules([
         '@PSR1'                       => true,
-        '@PSR12'                      => true,
-        'strict_param'                => true,
+        '@PSR2'                       => true,
         'array_syntax'                => ['syntax' => 'short'],
         'blank_line_before_statement' => ['statements' => ['return', 'throw']],
-        'general_phpdoc_tag_rename'   => ['replacements' => [
-            'inheritDocs' => 'inheritDoc',
-            'inheritdocs' => 'inheritDoc',
-            'inheritdoc'  => 'inheritDoc',
-        ]],
         'no_superfluous_phpdoc_tags' => [
             'allow_mixed'         => true,
-            'allow_unused_params' => true,
         ],
-        'no_unneeded_curly_braces' => [
-            'namespaces' => true,
-        ],
+        'no_unneeded_curly_braces' => true,
         'phpdoc_types_order' => [
             'null_adjustment' => 'always_last',
         ],
