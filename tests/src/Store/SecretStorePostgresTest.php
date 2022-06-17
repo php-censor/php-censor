6930f7d8701030abd0a1d2a7ac8a8c961997f784
Dmitry Khomutov <poisoncorpsee@gmail.com>
2022-06-17 10:46:49 +0700

    Improved DB tests.

--- tests/src/Store/SecretStorePostgresTest.php
+++ tests/src/Store/SecretStorePostgresTest.php
@@ -4,154 +4,96 @@ declare(strict_types=1);
 
 namespace Tests\PHPCensor\Store;
 
-use Phinx\Config\Config as PhinxConfig;
-use Phinx\Console\Command\Migrate;
-use PHPCensor\ArrayConfiguration;
-use PHPCensor\DatabaseManager;
 use PHPCensor\Model\Secret;
 use PHPCensor\Store;
 use PHPCensor\Store\SecretStore;
-use PHPCensor\StoreRegistry;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\OutputInterface;
+use Tests\PHPCensor\BasePostgresTestCase;
 
-class SecretStorePostgresTest extends TestCase
+class SecretStorePostgresTest extends BasePostgresTestCase
 {
-    private ?\PDO $connection = null;
-
-    private StoreRegistry $storeRegistry;
     private Store $store;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $configurationArray = [
-            'php-censor' => [
-                'database' => [
-                    'servers'  => [
-                        'read'  => [
-                            ['host' => '127.0.0.1'],
-                        ],
-                        'write' => [
-                            ['host' => '127.0.0.1'],
-                        ],
-                    ],
-                    'type'     => 'pgsql',
-                    'name'     => env('POSTGRESQL_DBNAME'),
-                    'username' => env('POSTGRESQL_USER'),
-                    'password' => env('POSTGRESQL_PASSWORD'),
+        $this->store = new SecretStore($this->databaseManager, $this->storeRegistry);
+    }
+
+    protected function getTestData(): array
+    {
+        return [
+            'users' => [
+                [
+                    'email'    => 'user1@test.test',
+                    'hash'     => 'abc1',
+                    'name'     => 'user 1',
+                    'is_admin' => 1,
+                ],
+                [
+                    'email'    => 'user2@test.test',
+                    'hash'     => 'abc2',
+                    'name'     => 'user 2',
+                    'is_admin' => 0,
+                ],
+                [
+                    'email'    => 'user3@test.test',
+                    'hash'     => 'abc3',
+                    'name'     => 'user 3',
+                    'is_admin' => 0,
+                ],
+                [
+                    'email'    => 'user4@test.test',
+                    'hash'     => 'abc4',
+                    'name'     => 'user 4',
+                    'is_admin' => 0,
+                ],
+            ],
+            'secrets' => [
+                [
+                    'name'        => 'secret 1',
+                    'value'       => 'value 1',
+                    'create_date' => '2014-01-01 01:01:00',
+                    'user_id'     => 1,
+                ],
+                [
+                    'name'        => 'secret 2',
+                    'value'       => 'value 2',
+                    'create_date' => '2015-01-01 01:01:00',
+                    'user_id'     => 1,
+                ],
+                [
+                    'name'        => 'secret 3',
+                    'value'       => 'value 3',
+                    'create_date' => '2016-01-01 01:01:00',
+                    'user_id'     => 1,
+                ],
+                [
+                    'name'        => 'secret 4',
+                    'value'       => 'value 4',
+                    'create_date' => '2017-01-01 01:01:00',
+                    'user_id'     => 1,
+                ],
+                [
+                    'name'        => 'secret 5',
+                    'value'       => 'value 5',
+                    'create_date' => '2018-01-01 01:01:00',
+                    'user_id'     => 2,
+                ],
+                [
+                    'name'        => 'secret 6',
+                    'value'       => 'value 6',
+                    'create_date' => '2018-02-01 01:01:00',
+                    'user_id'     => 3,
+                ],
+                [
+                    'name'        => 'secret 7',
+                    'value'       => 'value 7',
+                    'create_date' => '2018-03-01 01:01:00',
+                    'user_id'     => 4,
                 ],
             ],
         ];
-
-        try {
-            $this->connection = new \PDO(
-                'pgsql:host=127.0.0.1;dbname=' . env('POSTGRESQL_DBNAME'),
-                env('POSTGRESQL_USER'),
-                env('POSTGRESQL_PASSWORD')
-            );
-
-            $this->connection->exec('DROP TABLE IF EXISTS "migrations"');
-            $this->connection->exec('DROP TABLE IF EXISTS "webhook_requests"');
-            $this->connection->exec('DROP TABLE IF EXISTS "build_errors"');
-            $this->connection->exec('DROP TABLE IF EXISTS "build_metas"');
-            $this->connection->exec('DROP TABLE IF EXISTS "builds"');
-            $this->connection->exec('DROP TABLE IF EXISTS "environments"');
-            $this->connection->exec('DROP TABLE IF EXISTS "projects"');
-            $this->connection->exec('DROP TABLE IF EXISTS "project_groups"');
-            $this->connection->exec('DROP TABLE IF EXISTS "secrets"');
-            $this->connection->exec('DROP TABLE IF EXISTS "users"');
-
-            $phinxSettings = [
-                'paths' => [
-                    'migrations' => ROOT_DIR . 'src/Migrations',
-                ],
-                'environments' => [
-                    'default_migration_table' => 'migrations',
-                    'default_database'        => 'php-censor',
-                    'php-censor'              => [
-                        'adapter' => 'pgsql',
-                        'host' => '127.0.0.1',
-                        'name' => env('POSTGRESQL_DBNAME'),
-                        'user' => env('POSTGRESQL_USER'),
-                        'pass' => env('POSTGRESQL_PASSWORD'),
-                    ],
-                ],
-            ];
-            $phinxConfig = new PhinxConfig($phinxSettings);
-
-            try {
-                (new Migrate())
-                    ->setConfig($phinxConfig)
-                    ->setName('php-censor-migrations:migrate')
-                    ->run(new ArgvInput([]), new ConsoleOutput(OutputInterface::VERBOSITY_QUIET));
-            } catch (\Throwable $e) {
-                //var_dump($e);
-            }
-
-            $this->connection->exec("
-                INSERT INTO \"users\" (\"email\", \"hash\", \"name\", \"is_admin\") VALUES
-                ('user1@test.test', 'abc1', 'user 1', 1),
-                ('user2@test.test', 'abc2', 'user 2', 0),
-                ('user3@test.test', 'abc3', 'user 3', 0),
-                ('user4@test.test', 'abc4', 'user 4', 0)
-            ");
-
-            $this->connection->exec("
-                INSERT INTO \"secrets\" (\"name\", \"value\", \"create_date\", \"user_id\") VALUES
-                ('secret 1', 'value 1', '2014-01-01 01:01:00', 1),
-                ('secret 2', 'value 2', '2015-01-01 01:01:00', 1),
-                ('secret 3', 'value 3', '2016-01-01 01:01:00', 1),
-                ('secret 4', 'value 4', '2017-01-01 01:01:00', 1),
-                ('secret 5', 'value 5', '2018-01-01 01:01:00', 2),
-                ('secret 6', 'value 6', '2018-02-01 01:01:00', 3),
-                ('secret 7', 'value 7', '2018-03-01 01:01:00', 4)
-            ");
-        } catch (\Throwable $e) {
-            //var_dump($e);
-
-            $this->connection = null;
-        }
-
-        $this->getConnection();
-
-        $configuration       = new ArrayConfiguration($configurationArray);
-        $databaseManager     = new DatabaseManager($configuration);
-        $this->storeRegistry = new StoreRegistry($databaseManager);
-
-        $this->store = new SecretStore($databaseManager, $this->storeRegistry);
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        if (null !== $this->connection) {
-            $this->connection->exec('DROP TABLE IF EXISTS "migrations"');
-            $this->connection->exec('DROP TABLE IF EXISTS "webhook_requests"');
-            $this->connection->exec('DROP TABLE IF EXISTS "build_errors"');
-            $this->connection->exec('DROP TABLE IF EXISTS "build_metas"');
-            $this->connection->exec('DROP TABLE IF EXISTS "builds"');
-            $this->connection->exec('DROP TABLE IF EXISTS "environments"');
-            $this->connection->exec('DROP TABLE IF EXISTS "projects"');
-            $this->connection->exec('DROP TABLE IF EXISTS "project_groups"');
-            $this->connection->exec('DROP TABLE IF EXISTS "secrets"');
-            $this->connection->exec('DROP TABLE IF EXISTS "users"');
-
-            $this->connection = null;
-        }
-    }
-
-    protected function getConnection(): ?\PDO
-    {
-        if (null === $this->connection) {
-            $this->markTestSkipped('Test skipped because PostgreSQL database/user/extension doesn\'t exist.');
-        }
-
-        return $this->connection;
     }
 
     public function testGetByNamesSuccess(): void
