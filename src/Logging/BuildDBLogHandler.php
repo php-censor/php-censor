58ef53d3e316c7189858ec10221935d149a46d2c
Dmitry Khomutov <poisoncorpsee@gmail.com>
2022-08-30 12:21:20 +0700

    Fixes for secrets: hide secrets in the log.

--- src/Logging/BuildDBLogHandler.php
+++ src/Logging/BuildDBLogHandler.php
@@ -7,7 +7,9 @@ namespace PHPCensor\Logging;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 use PHPCensor\Model\Build;
+use PHPCensor\Model\Secret;
 use PHPCensor\Store\BuildStore;
+use PHPCensor\Store\SecretStore;
 
 /**
  * Class BuildDBLogHandler writes the build log to the database.
@@ -20,8 +22,8 @@ use PHPCensor\Store\BuildStore;
 class BuildDBLogHandler extends AbstractProcessingHandler
 {
     protected Build $build;
-
     protected BuildStore $buildStore;
+    private SecretStore $secretStore;
 
     protected string $logValue;
 
@@ -36,6 +38,7 @@ class BuildDBLogHandler extends AbstractProcessingHandler
     protected int $flushDelay = 1;
 
     public function __construct(
+        SecretStore $secretStore,
         BuildStore $buildStore,
         Build $build,
         int $level = Logger::INFO,
@@ -43,8 +46,9 @@ class BuildDBLogHandler extends AbstractProcessingHandler
     ) {
         parent::__construct($level, $bubble);
 
-        $this->build      = $build;
-        $this->buildStore = $buildStore;
+        $this->secretStore = $secretStore;
+        $this->build       = $build;
+        $this->buildStore  = $buildStore;
 
         // We want to add to any existing saved log information.
         $this->logValue = (string)$build->getLog();
@@ -66,17 +70,49 @@ class BuildDBLogHandler extends AbstractProcessingHandler
         $this->flushTimestamp = \time();
     }
 
+    private function sanitize(string $message): string
+    {
+        return \str_replace([
+            '\/',
+            '//',
+            $this->build->getBuildPath(),
+            ROOT_DIR,
+        ], [
+            '/',
+            '//',
+            '<BUILD_PATH>/',
+            '<PHP_CENSOR_PATH>/',
+        ], $message);
+    }
+
+    private function sanitizeSecrets(string $message): string
+    {
+        $replace = [];
+        $secrets = $this->secretStore->getAll();
+        if (\count($secrets['items']) > 0) {
+            /** @var Secret $secret */
+            foreach ($secrets['items'] as $secret) {
+                $value = $secret->getValue();
+                $name  = '%' . \sprintf('SECRET:%s', $secret->getName()) . '%';
+                if (\trim($value)) {
+                    $replace[$name] = $secret->getValue();
+                }
+            }
+        }
+
+        return \str_replace($replace, \array_keys($replace), $message);
+    }
+
     /**
      * Write a log entry to the build log.
      */
     protected function write(array $record): void
     {
-        $message = (string)$record['message'];
-        $message = \str_replace(['\/', '//'], '/', $message);
-        $message = \str_replace($this->build->getBuildPath(), '<BUILD_PATH>/', $message);
-        $message = \str_replace(ROOT_DIR, '<PHP_CENSOR_PATH>/', $message);
-
-        $this->logValue .= $message . PHP_EOL;
+        $this->logValue .= $this->sanitize(
+            $this->sanitizeSecrets(
+                (string)$record['message']
+            )
+        ) . PHP_EOL;
 
         if ($this->flushTimestamp < (\time() - $this->flushDelay)) {
             $this->flushData();
